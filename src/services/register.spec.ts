ddc95729b855d361539b828575f2c92764d8cf87
Gabriel Pelegrine <gp.pelegrine@gmail.com>
2024-05-07 16:35:37 -0300

    Work in progress - registerSpec validation

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,23 +1,30 @@
 import { describe, expect, it } from 'vitest'
 import { RegisterUserCase } from './register'
 import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '../repositories/in-memory/in-memory-users-repository'
 
 describe('Register use case', () => {
   it('A senha do usuário deve ser Hash', async () => {
-    const registerUserCase = new RegisterUserCase({
-      async findByEmail() {
-        return null
-      },
-      async create(data) {
-        return {
-          id: parseInt('user-1'),
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        }
-      },
+    const usersRepositoryInMemory = new InMemoryUsersRepository()
+    const registerUserCase = new RegisterUserCase(usersRepositoryInMemory)
+
+    const { user } = await registerUserCase.mutationCreate({
+      name: 'John',
+      email: 'John@example.com',
+      password: '1234567',
     })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '1234567',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('O e-mail já está cadastrado', async () => {
+    const usersRepositoryInMemory = new InMemoryUsersRepository()
+    const registerUserCase = new RegisterUserCase(usersRepositoryInMemory)
 
     const { user } = await registerUserCase.mutationCreate({
       name: 'John',
