0e938ba3428931eedd0bcd829aa59c14766170ae
Gabriel Pelegrine <gp.pelegrine@gmail.com>
2024-05-07 22:37:54 -0300

    Work in Progress - Added new test - Coverage

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ GymPass style app.
 
 ## RFs (Requisitos funcionas)
 
-- [ ] Deve ser possível se cadastrar;
+- [X] Deve ser possível se cadastrar;
 - [ ] Deve ser possível se autenticar;
 - [ ] Deve ser possível obter o perfil de um usuário logado;
 - [ ] Deve ser possível obter o número de check-ins realizados pelo usuário;
@@ -17,7 +17,7 @@ GymPass style app.
 
 ## RNs (Regras de negócio)
 
-- [ ] O usuário não deve poder se cadastrar com um e-mail duplicado;
+- [X] O usuário não deve poder se cadastrar com um e-mail duplicado;
 - [ ] O usuário não pode fazer 2 check-in no mesmo dia;
 - [ ] O usuário não fazer check-in se não estiver perto (100m) da academia;
 - [ ] O check-in só pode ser validado até 20 minutos após criado;
@@ -26,8 +26,8 @@ GymPass style app.
 
 ## RNFs (Requisitos não-funcionas) - (Técnico)
 
-- [ ] A senha do usuário precisar estar criptografada;
-- [ ] Os dados da aplicação precisam estar persistidos em um Banco de dados PostgreSQL;
+- [X] A senha do usuário precisar estar criptografada;
+- [X] Os dados da aplicação precisam estar persistidos em um Banco de dados PostgreSQL;
 - [ ] Todas listas de dados precisam estar paginadas com 20 itens por página;
 - [ ] O usuário deve ser identificado por um JWT(JSON Web Token)
 
--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -2,8 +2,22 @@ import { describe, expect, it } from 'vitest'
 import { RegisterUserCase } from './register'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '../repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './error/user-already-exists-error'
 
 describe('Register use case', () => {
+  it('Confirmação de cadastro', async () => {
+    const usersRepositoryInMemory = new InMemoryUsersRepository()
+    const registerUserCase = new RegisterUserCase(usersRepositoryInMemory)
+
+    const { user } = await registerUserCase.mutationCreate({
+      name: 'John',
+      email: 'John@example.com',
+      password: '1234567',
+    })
+
+    expect(user.id).toEqual(expect.any(Number))
+  })
+
   it('A senha do usuário deve ser Hash', async () => {
     const usersRepositoryInMemory = new InMemoryUsersRepository()
     const registerUserCase = new RegisterUserCase(usersRepositoryInMemory)
@@ -26,17 +40,20 @@ describe('Register use case', () => {
     const usersRepositoryInMemory = new InMemoryUsersRepository()
     const registerUserCase = new RegisterUserCase(usersRepositoryInMemory)
 
-    const { user } = await registerUserCase.mutationCreate({
+    const email = 'John@example.com'
+
+    await registerUserCase.mutationCreate({
       name: 'John',
-      email: 'John@example.com',
+      email,
       password: '1234567',
     })
 
-    const isPasswordCorrectlyHashed = await compare(
-      '1234567',
-      user.password_hash,
-    )
-
-    expect(isPasswordCorrectlyHashed).toBe(true)
+    await expect(() =>
+      registerUserCase.mutationCreate({
+        name: 'John',
+        email,
+        password: '1234567',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
