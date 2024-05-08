2879f293222dff0def7738ce00bdd876dd90a2ee
Gabriel Pelegrine <gp.pelegrine@gmail.com>
2024-05-08 11:04:56 -0300

    Invalid Credentials Auth

---
+++ src/services/authenticate.ts
@@ -0,0 +1,38 @@
+import { compare } from 'bcryptjs'
+import { UsersRepository } from '../repositories/users-repository'
+import { InvalidCredentialsError } from './error/invalid-credentials-error'
+import { User } from '@prisma/client'
+
+interface AuthenticateServiceRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateServiceResponse {
+  user: User
+}
+
+export class AuthenticateService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateServiceRequest): Promise<AuthenticateServiceResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
