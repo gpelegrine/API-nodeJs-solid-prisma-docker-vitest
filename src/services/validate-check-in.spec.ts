c7fbc37fc455e55d7ecd57d6a7476fb6ce517c43
Gabriel Pelegrine <gp.pelegrine@gmail.com>
2024-05-21 18:17:50 -0300

    wip - Validate checkIn

---
+++ src/services/validate-check-in.spec.ts
@@ -0,0 +1,73 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInRepository } from '../repositories/checkin-repository'
+import { GymsRepository } from '../repositories/gyms-repository'
+import { ResourceNotFoundError } from './error/resource-not-found-error'
+import { getDistanceBetweenCoordinates } from './utils/get-distance-between-coordinates'
+import { MaxDistanceError } from './error/max-distance-error'
+import { MaxNumberOfCheckInsError } from './error/max-number-of-checkIns-error'
+
+interface ValidateCheckInServiceRequest {
+  userId: number
+  gymId: number
+  userLatitude: number
+  userLongitude: number
+}
+
+interface ValidateCheckInServiceResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInService {
+  constructor(
+    private checkinRepository: CheckInRepository,
+    private gymsRepository: GymsRepository,
+  ) {}
+
+  async execute({
+    gymId,
+    userId,
+    userLatitude,
+    userLongitude,
+  }: ValidateCheckInServiceRequest): Promise<ValidateCheckInServiceResponse> {
+    const gym = await this.gymsRepository.findByUserId(gymId)
+
+    if (!gym) {
+      throw new ResourceNotFoundError()
+    }
+
+    const distance = getDistanceBetweenCoordinates(
+      {
+        latitude: userLatitude,
+        longitude: userLongitude,
+      },
+      {
+        latitude: Number(gym.latitude),
+        longitude: Number(gym.longitude),
+      },
+    )
+
+    const MAX_DISTANCE_KM = 0.1
+
+    if (distance > MAX_DISTANCE_KM) {
+      throw new MaxDistanceError()
+    }
+
+    const checkInOnSameDay = await this.checkinRepository.findByUserIdOnDate(
+      userId,
+      new Date(),
+    )
+
+    if (checkInOnSameDay) {
+      throw new MaxNumberOfCheckInsError()
+    }
+
+    const checkIn = await this.checkinRepository.create({
+      gym_id: gymId,
+      user_id: userId,
+    })
+
+    return {
+      checkIn,
+    }
+  }
+}
