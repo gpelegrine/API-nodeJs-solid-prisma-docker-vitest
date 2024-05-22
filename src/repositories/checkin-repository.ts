4b28f8072c2bde0085d59a83373522c6d7ce5562
Gabriel Pelegrine <gp.pelegrine@gmail.com>
2024-05-21 23:17:32 -0300

    Wip - validate checkIns

--- src/repositories/checkin-repository.ts
+++ src/repositories/checkin-repository.ts
@@ -2,7 +2,9 @@ import { CheckIn, Prisma } from '@prisma/client'
 
 export interface CheckInRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  countByUserId(userId: number): Promise<number>
   findByUserIdOnDate(userId: number, date: Date): Promise<CheckIn | null>
   findManyByUserHistory(userId: number, pageInfo: number): Promise<CheckIn[]>
-  countByUserId(userId: number): Promise<number>
+  findById(id: number): Promise<CheckIn | null>
+  save(CheckIn: CheckIn): Promise<CheckIn>
 }
--- src/repositories/in-memory/in-memory-checkin-repository.ts
+++ src/repositories/in-memory/in-memory-checkin-repository.ts
@@ -6,6 +6,16 @@ import dayjs from 'dayjs'
 export class InMemoryCheckInRepository implements CheckInRepository {
   items: CheckIn[] = []
 
+  async findById(id: number) {
+    const checkIn = this.items.find((item) => item.id === id)
+
+    if (!checkIn) {
+      return null
+    }
+
+    return checkIn
+  }
+
   async findByUserIdOnDate(userId: number, date: Date) {
     const startOfTheDay = dayjs(date).startOf('date')
     const endOfTheDay = dayjs(date).endOf('date')
@@ -45,6 +55,16 @@ export class InMemoryCheckInRepository implements CheckInRepository {
     }
 
     this.items.push(checkIn)
+
+    return checkIn
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id)
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn
+    }
 
     return checkIn
   }
